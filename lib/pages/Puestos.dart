1e0d2426e555d2e519731b53e7027522ce31a347
OscarJaVaz <oscarjandetevazquez@gmail.com>
2024-02-11 18:27:23 -0600

    Tabla Puestos

---
+++ lib/pages/Puestos.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Puestos extends StatefulWidget {
+  const Puestos({super.key});
+
+  @override
+  State<Puestos> createState() => _PuestosState();
+}
+
+class _PuestosState extends State<Puestos> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
